485b717b7eb2105492f545070559b0d11296f358
Mitchell Walker <eagleye1144@gmail.com>
2021-11-01 19:05:44 -0600

    Almost done fixing bugs

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,30 +1,40 @@
 const router = require('express').Router()
+const md = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
  try {
-   res.json('get accounts')
+   const accounts = await Account.getAll()
+   res.json(accounts)
  } catch (error) {
    next(error)
  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', md.checkAccountId , async  (req, res, next) => {
+   res.json(req.accounts)
+})
+
+router.post('/', 
+ md.checkAccountPayload ,
+ md.checkAccountNameUnique,
+ async (req, res, next) => {
  try {
-   res.json('get accounts by id')
+   const newAccount = await Account.create(
+    { name: req.body.name.trim(),
+    budget: req.body.budget })
+   res.status(201).json(newAccount)
  } catch (error) {
    next(error)
  }
 })
 
-router.post('/', (req, res, next) => {
- try {
-   res.json('post account')
- } catch (error) {
-   next(error)
- }
-})
-
-router.put('/:id', (req, res, next) => {
+router.put('/:id',
+ md.checkAccountId,
+ md.checkAccountPayload,
+  async (req, res, next) => {
+    const updated = await Account.updateById(req.params.id, req.body)
+    res.json(updated)
  try {
    res.json('update account')
  } catch (error) {
@@ -32,9 +42,10 @@ router.put('/:id', (req, res, next) => {
  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
  try {
-   res.json('delete accounts')
+   await Account.deleteById(req.params.id)
+   res.json(req.account)
  } catch (error) {
    next(error)
  }
